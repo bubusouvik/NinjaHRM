2d979521faa740316d9ebbc8c34ffd901a42fcd4
Souvik Guria <59893388+bubusouvik@users.noreply.github.com>
2025-08-28 15:41:08 +0530

    one code is added

--- src/main/java/com/ninjacrm/webdriverutility/WebdriverUtility.java
+++ src/main/java/com/ninjacrm/webdriverutility/WebdriverUtility.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+// webdriver utility method
 public class WebdriverUtility {
 
 	public void maximizeScreen(WebDriver driver) {
